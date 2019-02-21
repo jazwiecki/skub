12d533526c339d56de3941a6fa5e29bde01945f9
Jim Jazwiecki <jim.jazwiecki@gmail.com>
2019-02-21 16:59:03 -0500

    add TODOs

--- uniform/tests.py
+++ uniform/tests.py
@@ -1,5 +1,10 @@
 import uniform
 
+# TODO: test excluding non-alpha chars
+# TODO: refactor uniform to take a set of regexes for exclusion/replacement?
+# TODO: test @-mentions
+# TODO: test #channels
+
 def test_transform():
     expected_string = "𝕒𝕓𝕔"
     test_submission = "abc"
